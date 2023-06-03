9d75dc078dd42fd732c4a4a61167a23bf3dcd897
harvy666 <harvyhun@gmail.com>
2023-06-03 19:16:51 +0200

    refactored datepicker to use separate script.js file instead of inline code in the Pug

--- public/script.js
+++ public/script.js
@@ -1,19 +1,14 @@
-// const controller = require("./controller");
-
-// document.querySelector(".btn-press").addEventListener("click");
-
-//import { getStudents } from "./controller";
-import { getStudents } from "./controller";
-
-let button = document.querySelector(".btn-press");
-button.addEventListener("click", () => {
-  getStudents((error, students) => {
-    if (error) {
-      console.error(error);
-      // handle the error in some way
-    } else {
-      // do something with the students array
-      console.log(students);
-    }
-  });
+const input = document.querySelector("#datepicker");
+const maxDate = new Date();
+maxDate.setDate(maxDate.getDate() + 7);
+flatpickr(input, {
+  //defaultDate: 'today',
+  minDate: "today",
+  maxDate: maxDate,
+  onChange: function (selectedDates, dateStr, instance) {
+    console.log("Hello from flat");
+    console.log("Selected date:", selectedDates[0]);
+    console.log("Formatted date string:", dateStr);
+    console.log("Flatpickr instance:", instance);
+  },
 });
