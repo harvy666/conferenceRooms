30e21cef28daa6012b0c195dc4b554e632a9e9b4
harvy666 <harvyhun@gmail.com>
2023-06-04 21:15:37 +0200

    add check if todays date is equal to selected date (will help with Postgres later I guess)

--- public/script.js
+++ public/script.js
@@ -2,8 +2,21 @@
 const input = document.querySelector("#datepicker");
 const maxDate = new Date();
 maxDate.setDate(maxDate.getDate() + 7);
+
+//testing date
+
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+const day = String(currentDate.getDate()).padStart(2, "0");
+
+//format to datepicker and Postgres style
+const formattedDate = `${year}-${month}-${day}`;
+
+let simpleDate;
 flatpickr(input, {
   //defaultDate: 'today',
+
   locale: {
     firstDayOfWeek: 1,
   },
@@ -12,6 +25,11 @@ flatpickr(input, {
   onChange: function (selectedDates, dateStr, instance) {
     console.log("Hello from flat");
     console.log("Selected date:", selectedDates[0]);
-    console.log("Formatted date string:", dateStr);
+    simpleDate = dateStr;
+    console.log("Formatted date string:", simpleDate);
+    //testing date in datepicker
+    if (formattedDate === simpleDate) {
+      console.log("Ma van!");
+    } else console.log("Nem ma van :(");
   },
 });
