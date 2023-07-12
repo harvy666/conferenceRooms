67c5d9e4eb2186633c9f543689c6fe4c6303e1e9
harvy666 <harvyhun@gmail.com>
2023-07-12 13:51:39 +0200

    minor cleanup

--- public/script.js
+++ public/script.js
@@ -1,19 +1,8 @@
 //datepicker code
+let simpleDate;
 const input = document.querySelector("#datepicker");
 const maxDate = new Date();
 maxDate.setDate(maxDate.getDate() + 7);
-
-//testing date
-
-const currentDate = new Date();
-const year = currentDate.getFullYear();
-const month = String(currentDate.getMonth() + 1).padStart(2, "0");
-const day = String(currentDate.getDate()).padStart(2, "0");
-
-//format to datepicker and Postgres style
-const formattedDate = `${year}-${month}-${day}`;
-
-let simpleDate;
 
 document.addEventListener("DOMContentLoaded", () => {
   flatpickr(input, {
