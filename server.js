7b94df908ca5d9901f78accfd2f4488faed71862
harvy666 <harvyhun@gmail.com>
2024-01-14 20:28:39 +0100

    cleaning up before any real work

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
 //controller feletti réteg?
+//const queries = require("./public/queries");
+//const studentRoutes = require("./public/routes");
 
 const express = require("express");
-const studentRoutes = require("./public/routes");
 const path = require("path");
 const pool = require("./db");
-const queries = require("./public/queries");
-
 const app = express();
 const port = 3000;
 
+// incoming  json parsing (accessible  on the req.body later?)
 app.use(express.json());
 //add static files
 app.use(express.static(path.join(__dirname, "public")));
-
-// add path to index
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.use("/", studentRoutes);
-
 app.listen(port, () => console.log(`app listening on port ${port}`));
 
 //////////////
